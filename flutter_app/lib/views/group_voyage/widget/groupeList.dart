fc19f2a687fcc06a9829dadfbc81f1b90208eb1b
eddyg <eddygomet@gmail.com>
2024-07-11 11:54:57 +0200

    .style liste de groupes

--- flutter_app/lib/views/group_voyage/widget/groupeList.dart
+++ flutter_app/lib/views/group_voyage/widget/groupeList.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/group_voyage/widget/group_items.dart';
 import 'package:flutter_app/views/groupe_detail/groupe_detail_screen.dart';
+import 'dart:math';
 import '../../../models/groupe_model.dart';
 
 class Groupelist extends StatelessWidget {
@@ -10,20 +12,33 @@ class Groupelist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      scrollDirection: Axis.vertical,
       itemCount: groupes.length,
       itemBuilder: (context, i) {
         var groupe = groupes[i];
-        return ListTile(
-          title: Text(groupe.nom),
-          subtitle: Text(groupe.budget?.toString() ?? 'Pas de budget'),
+        return GroupItems(
+          nom: groupe.nom,
+          budget: groupe.budget?.toString() ?? 'Pas de budget',
+          color: getRandomColor(),
           onTap: () => Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context)=> GroupeDetailScreen(groupeId: groupe.id!),
-            )
+              builder: (context) => GroupeDetailScreen(groupeId: groupe.id!),
+            ),
           ),
         );
       },
+    );
+  }
+
+  // Génère une couleur aléatoire
+  Color getRandomColor() {
+    final Random random = Random();
+    return Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
     );
   }
 }
